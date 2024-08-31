6200a4abcbc5486c4e6c63532679f559e10c9127
Asaf Marom <asafmarom89@gmail.com>
2024-08-31 17:13:57 +0300

    chore: Remove commented out code in chatwootToWhapi.js

--- api/chatwootToWhapi.js
+++ api/chatwootToWhapi.js
@@ -66,68 +66,6 @@ router.post('/', async (req, res) => {
 
     return;
 });
-////////////////////////
-
-// router.post('/', async (req, res) => {
-//     console.log("inside and starting chatwootToWhapi route");
-
-//     let labels = "";
-//     let attachmentFlag = false;
-//     const chatwootMessage = req.body;
-
-//     if (!chatwootMessage) {
-//         console.log("no chatwoot message");
-//         return res.status(400).send('No chatwoot message provided');
-//     }
-
-//     // Respond immediately
-//     res.status(200).send('Processing started');
-
-//     // Background processing
-//     (async () => {
-//         try {
-//             if (chatwootMessage.attachments && chatwootMessage.attachments.length > 0) {
-//                 attachmentFlag = true;
-//             }
-
-//             if (chatwootMessage.message_type === 'incoming') {
-//                 console.log("incoming message");
-//                 return;
-//             }
-
-//             if (!chatwootMessage.conversation) {
-//                 console.log("no chatwoot message");
-//                 return;
-//             }
-
-//             if (chatwootMessage.conversation.labels) {
-//                 labels = chatwootMessage.conversation.labels[0];
-//             }
-
-//             const recipientPhoneNumber = chatwootMessage.conversation.meta.sender.phone_number;
-//             const formatedNumber = formatPhoneNumber(recipientPhoneNumber);
-//             let convertedChannelName = getChannelInfoByLabel(labels);
-
-//             if (attachmentFlag) {
-//                 for (let attachment of chatwootMessage.attachments) {
-//                     let file_type = attachment.file_type;
-//                     let data_url = attachment.data_url;
-//                     await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, file_type, data_url);
-//                 }
-//                 console.log("finished loop de loop");
-//                 return;
-//             }
-
-//             await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, "text", "");
-
-//         } catch (error) {
-//             console.error('Error sending message to WhatsApp:', error);
-//         }
-
-//         console.log("finished chatwootToWhapi route");
-//     })();
-// });
-
 
 module.exports = router;
 
