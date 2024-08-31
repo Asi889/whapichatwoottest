2dcbf7c75fd19a58ba4867de30c377f6336e10a9
Asaf Marom <asafmarom89@gmail.com>
2024-08-31 17:01:29 +0300

    checking chatwoottowhapi fix log vercel

--- api/chatwootToWhapi.js
+++ api/chatwootToWhapi.js
@@ -5,67 +5,126 @@ const whapisendmsg = require('../whapiendpoint/whapisendmsg');
 const channels = require('../utils/channels');
 const getChannelInfoByLabel = require('../utils/getChannelInfoByLabel');
 const formatPhoneNumber = require('../utils/formatPhoneNumber');
-const { text } = require('body-parser');
 const router = express.Router();
 
+// router.post('/', async (req, res) => {
+//     console.log("inside and starting chatwootToWhapi route");
+
+//     let labels = "";
+//     let attachmentFlag = false
+//     const chatwootMessage = req.body;
+
+//     if (!chatwootMessage) {
+//         console.log("no chatwoot message");
+//         return;
+//     }
+//     if (chatwootMessage.attachments && chatwootMessage.attachments.length > 0) {
+//         attachmentFlag = true;
+//     }
+
+//     if (chatwootMessage.message_type === 'incoming') {
+//         console.log("incoming message");
+//         return;
+//     };
+
+//     if (!chatwootMessage.conversation) {
+//         console.log("no chatwoot message");
+//         return;
+//     };
+
+//     if (chatwootMessage.conversation.labels) {
+//         labels = chatwootMessage?.conversation?.labels[0];
+//     };
+
+//     const recipientPhoneNumber = chatwootMessage.conversation.meta.sender.phone_number;
+
+//     const formatedNumber = formatPhoneNumber(recipientPhoneNumber);
+
+//     let convertedChannelName = getChannelInfoByLabel(labels);
+
+//     try {
+//         if (attachmentFlag) {
+
+//             for (let attachment of chatwootMessage.attachments) {
+//                 let file_type = attachment.file_type;
+//                 let data_url = attachment.data_url;
+//                 await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, file_type, data_url);
+//             }
+//             console.log("finished loop de loop");
+
+//             return;
+//         }
+
+//         await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, "text", "");
+
+//     } catch (error) {
+//         console.error('Error sending message to WhatsApp:', error);
+//         res.status(500).send('Error processing Chatwoot message');
+//     }
+//     console.log("finished chatwootToWhapi route");
+
+//     return;
+// });
 router.post('/', async (req, res) => {
     console.log("inside and starting chatwootToWhapi route");
 
     let labels = "";
-    let attachmentFlag = false
+    let attachmentFlag = false;
     const chatwootMessage = req.body;
 
     if (!chatwootMessage) {
         console.log("no chatwoot message");
-        return;
-    }
-    if (chatwootMessage.attachments && chatwootMessage.attachments.length > 0) {
-        attachmentFlag = true;
+        return res.status(400).send('No chatwoot message provided');
     }
 
-    if (chatwootMessage.message_type === 'incoming') {
-        console.log("incoming message");
-        return;
-    };
+    // Respond immediately
+    res.status(200).send('Processing started');
 
-    if (!chatwootMessage.conversation) {
-        console.log("no chatwoot message");
-        return;
-    };
-
-    if (chatwootMessage.conversation.labels) {
-        labels = chatwootMessage?.conversation?.labels[0];
-    };
-
-    const recipientPhoneNumber = chatwootMessage.conversation.meta.sender.phone_number;
-
-    const formatedNumber = formatPhoneNumber(recipientPhoneNumber);
-
-    let convertedChannelName = getChannelInfoByLabel(labels);
-
-    try {
-        if (attachmentFlag) {
-
-            for (let attachment of chatwootMessage.attachments) {
-                let file_type = attachment.file_type;
-                let data_url = attachment.data_url;
-                await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, file_type, data_url);
+    // Background processing
+    (async () => {
+        try {
+            if (chatwootMessage.attachments && chatwootMessage.attachments.length > 0) {
+                attachmentFlag = true;
             }
-            console.log("finished loop de loop");
 
-            return;
+            if (chatwootMessage.message_type === 'incoming') {
+                console.log("incoming message");
+                return;
+            }
+
+            if (!chatwootMessage.conversation) {
+                console.log("no chatwoot message");
+                return;
+            }
+
+            if (chatwootMessage.conversation.labels) {
+                labels = chatwootMessage.conversation.labels[0];
+            }
+
+            const recipientPhoneNumber = chatwootMessage.conversation.meta.sender.phone_number;
+            const formatedNumber = formatPhoneNumber(recipientPhoneNumber);
+            let convertedChannelName = getChannelInfoByLabel(labels);
+
+            if (attachmentFlag) {
+                for (let attachment of chatwootMessage.attachments) {
+                    let file_type = attachment.file_type;
+                    let data_url = attachment.data_url;
+                    await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, file_type, data_url);
+                }
+                console.log("finished loop de loop");
+                return;
+            }
+
+            await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, "text", "");
+
+        } catch (error) {
+            console.error('Error sending message to WhatsApp:', error);
         }
 
-        await whapisendmsg(formatedNumber, chatwootMessage, convertedChannelName.apiKey, "text", "");
-
-    } catch (error) {
-        console.error('Error sending message to WhatsApp:', error);
-        res.status(500).send('Error processing Chatwoot message');
-    }
-    console.log("finished chatwootToWhapi route");
-
-    return;
+        console.log("finished chatwootToWhapi route");
+    })();
 });
+
 
 module.exports = router;
 
